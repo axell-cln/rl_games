f70ed5e49d3373293963b68630a03a9cb6ee9501
AntoineRichard <antoine.richard@uni.lu>
2023-05-31 21:57:50 +0200

    added support for sine/cosine state encoding.

--- rl_games/algos_torch/network_builder.py
+++ rl_games/algos_torch/network_builder.py
@@ -9,7 +9,7 @@ import torch.optim as optim
 import math
 import numpy as np
 from rl_games.algos_torch.d2rl import D2RLNet
-from rl_games.algos_torch.transformer import TransformerModel, MetamorphModel
+from rl_games.algos_torch.transformer import TransformerModel, MetamorphModel, StatePositionalEncoder
 from rl_games.algos_torch.sac_helper import  SquashedNormal
 from rl_games.common.layers.recurrent import  GRUWithDones, LSTMWithDones
 from rl_games.common.layers.value import  TwoHotEncodedValue, DefaultValue
@@ -1264,7 +1264,11 @@ class MLPDictA2CBuilder(NetworkBuilder):
             self.actor_mlp = nn.Sequential()
             self.critic_mlp = nn.Sequential()
 
-            mlp_input_shape = input_shape['state'][0]
+            if self.use_state_pe:
+                self.encoding = StatePositionalEncoder(input_shape['state'][0], self.pe_nfreq, log_space = self.pe_log)
+                mlp_input_shape = self.encoding.d_output
+            else:
+                mlp_input_shape = input_shape['state'][0]
 
             in_mlp_shape = mlp_input_shape
             if len(self.units) == 0:
@@ -1323,8 +1327,13 @@ class MLPDictA2CBuilder(NetworkBuilder):
             obs = obs_dict['obs']
             states = obs_dict.get('rnn_states', None)
 
+            if self.use_state_pe:
+                inputs = self.encoding(obs['state'])
+            else:
+                inputs = obs['state']
+
             if self.separate:
-                a_out = c_out = obs['state']
+                a_out = c_out = inputs
                 a_out = self.actor_mlp(a_out)
                 c_out = self.critic_mlp(c_out)
                             
@@ -1347,7 +1356,7 @@ class MLPDictA2CBuilder(NetworkBuilder):
 
                     return mu, sigma, value, states
             else:
-                out = obs['state']
+                out = inputs
                 out = self.actor_mlp(out)
                 value = self.value_act(self.value(out))
 
@@ -1391,6 +1400,9 @@ class MLPDictA2CBuilder(NetworkBuilder):
             self.has_space = 'space' in params
             self.central_value = params.get('central_value', False)
             self.joint_obs_actions_config = params.get('joint_obs_actions', None)
+            self.use_state_pe = params.get('use_state_encoding', False)
+            self.pe_nfreq = params.get('pe_nfreq', 6)
+            self.pe_log = params.get('pe_log', False)
 
             if self.has_space:
                 self.is_multi_discrete = 'multi_discrete'in params['space']
@@ -1410,4 +1422,184 @@ class MLPDictA2CBuilder(NetworkBuilder):
 
     def build(self, name, **kwargs):
         net = MLPDictA2CBuilder.Network(self.params, **kwargs)
+        return net
+
+class MLPDictThrusterA2CBuilder(NetworkBuilder):
+    def __init__(self, **kwargs):
+        NetworkBuilder.__init__(self)
+
+    def load(self, params):
+        self.params = params
+
+    class Network(NetworkBuilder.BaseNetwork):
+        def __init__(self, params, **kwargs):
+            actions_num = kwargs.pop('actions_num')
+            input_shape = kwargs.pop('input_shape')
+            self.value_size = kwargs.pop('value_size', 1)
+            self.num_seqs = num_seqs = kwargs.pop('num_seqs', 1)
+            NetworkBuilder.BaseNetwork.__init__(self)
+            self.load(params)
+            self.actor_mlp = nn.Sequential()
+            self.critic_mlp = nn.Sequential()
+
+            if self.use_state_pe:
+                self.encoding = StatePositionalEncoder(input_shape['state'][0], self.pe_nfreq, log_space = self.pe_log)
+                mlp_input_shape = self.encoding.d_output
+            else:
+                mlp_input_shape = input_shape['state'][0]
+
+            mlp_input_shape = mlp_input_shape + input_shape['masks'][0]
+
+            in_mlp_shape = mlp_input_shape
+            if len(self.units) == 0:
+                out_size = mlp_input_shape
+            else:
+                out_size = self.units[-1]
+
+            mlp_args = {
+                'input_size' : in_mlp_shape, 
+                'units' : self.units, 
+                'activation' : self.activation, 
+                'norm_func_name' : self.normalization,
+                'dense_func' : torch.nn.Linear,
+                'd2rl' : self.is_d2rl,
+                'norm_only_first_layer' : self.norm_only_first_layer
+            }
+            self.actor_mlp = self._build_mlp(**mlp_args)
+            if self.separate:
+                self.critic_mlp = self._build_mlp(**mlp_args)
+
+            self.value = self._build_value_layer(out_size, self.value_size)
+            self.value_act = self.activations_factory.create(self.value_activation)
+
+            if self.is_discrete:
+                self.logits = torch.nn.Linear(out_size, actions_num)
+            if self.is_multi_discrete:
+                self.logits = torch.nn.ModuleList([torch.nn.Linear(out_size, num) for num in actions_num])
+            if self.is_continuous:
+                self.mu = torch.nn.Linear(out_size, actions_num)
+                self.mu_act = self.activations_factory.create(self.space_config['mu_activation']) 
+                mu_init = self.init_factory.create(**self.space_config['mu_init'])
+                self.sigma_act = self.activations_factory.create(self.space_config['sigma_activation']) 
+                sigma_init = self.init_factory.create(**self.space_config['sigma_init'])
+
+                if self.fixed_sigma:
+                    self.sigma = nn.Parameter(torch.zeros(actions_num, requires_grad=True, dtype=torch.float32), requires_grad=True)
+                else:
+                    self.sigma = torch.nn.Linear(out_size, actions_num)
+
+            mlp_init = self.init_factory.create(**self.initializer)
+
+            for m in self.modules():         
+                if isinstance(m, nn.Linear):
+                    mlp_init(m.weight)
+                    if getattr(m, "bias", None) is not None:
+                        torch.nn.init.zeros_(m.bias)    
+
+            if self.is_continuous:
+                mu_init(self.mu.weight)
+                if self.fixed_sigma:
+                    sigma_init(self.sigma)
+                else:
+                    sigma_init(self.sigma.weight)  
+
+        def forward(self, obs_dict):
+            obs = obs_dict['obs']
+            states = obs_dict.get('rnn_states', None)
+
+            if self.use_state_pe:
+                inputs = self.encoding(obs['state'])
+            else:
+                inputs = obs['state']
+
+            if self.separate:
+                a_out = c_out = torch.cat([inputs, obs['masks']],-1)
+                a_out = self.actor_mlp(a_out)
+                c_out = self.critic_mlp(c_out)
+                            
+                value = self.value_act(self.value(c_out))
+
+                if self.is_discrete:
+                    logits = self.logits(a_out)
+                    return logits, value, states
+
+                if self.is_multi_discrete:
+                    logits = [logit(a_out) for logit in self.logits]
+                    return logits, value, states
+
+                if self.is_continuous:
+                    mu = self.mu_act(self.mu(a_out))
+                    if self.fixed_sigma:
+                        sigma = mu * 0.0 + self.sigma_act(self.sigma)
+                    else:
+                        sigma = self.sigma_act(self.sigma(a_out))
+
+                    return mu, sigma, value, states
+            else:
+                out = torch.cat([inputs, obs['masks']],-1)
+                out = self.actor_mlp(out)
+                value = self.value_act(self.value(out))
+
+                if self.central_value:
+                    return value, states
+
+                if self.is_discrete:
+                    logits = self.logits(out)
+                    return logits, value, states
+                if self.is_multi_discrete:
+                    logits = [logit(out) for logit in self.logits]
+                    return logits, value, states
+                if self.is_continuous:
+                    mu = self.mu_act(self.mu(out))
+                    if self.fixed_sigma:
+                        sigma = self.sigma_act(self.sigma)
+                    else:
+                        sigma = self.sigma_act(self.sigma(out))
+                    return mu, mu*0 + sigma, value, states
+                    
+        def is_separate_critic(self):
+            return self.separate
+
+        def is_rnn(self):
+            return self.has_rnn
+
+        def get_default_rnn_state(self):
+            if not self.has_rnn:
+                return None
+
+        def load(self, params):
+            self.separate = params.get('separate', False)
+            self.units = params['mlp']['units']
+            self.activation = params['mlp']['activation']
+            self.initializer = params['mlp']['initializer']
+            self.is_d2rl = False
+            self.norm_only_first_layer = params['mlp'].get('norm_only_first_layer', False)
+            self.value_activation = params.get('value_activation', 'None')
+            self.normalization = params.get('normalization', None)
+            self.has_rnn = False
+            self.has_space = 'space' in params
+            self.central_value = params.get('central_value', False)
+            self.joint_obs_actions_config = params.get('joint_obs_actions', None)
+            self.use_state_pe = params.get('use_state_encoding', False)
+            self.pe_nfreq = params.get('pe_nfreq', 6)
+            self.pe_log = params.get('pe_log', False)
+
+            if self.has_space:
+                self.is_multi_discrete = 'multi_discrete'in params['space']
+                self.is_discrete = 'discrete' in params['space']
+                self.is_continuous = 'continuous'in params['space']
+                if self.is_continuous:
+                    self.space_config = params['space']['continuous']
+                    self.fixed_sigma = self.space_config['fixed_sigma']
+                elif self.is_discrete:
+                    self.space_config = params['space']['discrete']
+                elif self.is_multi_discrete:
+                    self.space_config = params['space']['multi_discrete']
+            else:
+                self.is_discrete = False
+                self.is_continuous = False
+                self.is_multi_discrete = False
+
+    def build(self, name, **kwargs):
+        net = MLPDictThrusterA2CBuilder.Network(self.params, **kwargs)
         return net
--- rl_games/algos_torch/transformer.py
+++ rl_games/algos_torch/transformer.py
@@ -405,6 +405,32 @@ class MetamorphModel(nn.Module):
         output = output.permute(1, 0, 2)
         return output, attention_maps, batch_size
 
+class StatePositionalEncoder(nn.Module):
+    r"""
+    Sine-cosine positional encoder for input points.
+    """
+    def __init__(self, d_input: int, n_freqs: int, log_space: bool = False):
+        super().__init__()
+        self.d_input = d_input
+        self.n_freqs = n_freqs
+        self.log_space = log_space
+        self.d_output = d_input * (1 + 2 * self.n_freqs)
+        self.embed_fns = [lambda x: x]
+
+        # Define frequencies in either linear or log scale
+        if self.log_space:
+            freq_bands = 2.**torch.linspace(0., self.n_freqs - 1, self.n_freqs)
+        else:
+            freq_bands = torch.linspace(2.**0., 2.**(self.n_freqs - 1), self.n_freqs)
+
+        # Alternate sin and cos
+        for freq in freq_bands:
+            self.embed_fns.append(lambda x, freq=freq: torch.sin(x * freq))
+            self.embed_fns.append(lambda x, freq=freq: torch.cos(x * freq))
+    
+    def forward(self, x) -> torch.Tensor:
+        return torch.concat([fn(x) for fn in self.embed_fns], dim=-1)
+
 class PositionalEncoding(nn.Module):
     def __init__(self, d_model, seq_len, dropout=0.1):
         super().__init__()
